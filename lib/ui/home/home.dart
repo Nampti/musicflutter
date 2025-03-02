4519f2b3b766047c02056cc209c2e7b0d2b2f09f
Nampti <nguyenhoainama3k55@gmail.com>
2025-03-02 20:21:33 +0700

    UPDATE

--- lib/ui/home/home.dart
+++ lib/ui/home/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:musicflutter/data/modal/song.dart';
 import 'package:musicflutter/ui/discovery/discovery.dart';
+import 'package:musicflutter/ui/home/viewmodal.dart';
 import 'package:musicflutter/ui/settings/settings.dart';
 import 'package:musicflutter/ui/user/user.dart';
 
@@ -12,10 +14,10 @@ class MusicAppp extends StatelessWidget {
     return MaterialApp(
       title: 'Music App',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
         useMaterial3: true,
       ),
-      home: MusicHomePage(),
+      home: const MusicHomePage(),
     );
   }
 }
@@ -33,12 +35,12 @@ class _MusicHomePageState extends State<MusicHomePage> {
     const DiscoveryTab(),
     const AccountTab(),
     const SettingsTab(),
-    
   ];
+
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(middle: Text('Music App')),
+      navigationBar: const CupertinoNavigationBar(middle: Text('Music App')),
       child: CupertinoTabScaffold(
         tabBar: CupertinoTabBar(
           backgroundColor: Theme.of(context).colorScheme.onInverseSurface,
@@ -56,15 +58,87 @@ class _MusicHomePageState extends State<MusicHomePage> {
     );
   }
 }
+
 class HomeTab extends StatelessWidget {
   const HomeTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Home Tab'),
-      ),
-      );
+    return const HomeTabPage();
+  }
+}
+
+class HomeTabPage extends StatefulWidget {
+  const HomeTabPage({super.key});
+
+  @override
+  State<HomeTabPage> createState() => _HomeTabPageState();
+}
+
+class _HomeTabPageState extends State<HomeTabPage> {
+  List<Song> songs = [];
+  late MusicAppViewModal _viewModal;
+
+  @override
+  void initState() {
+    super.initState();
+    _viewModal = MusicAppViewModal();
+    _viewModal.loadSong();
+    observeData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: getBody(),
+    );
+  }
+
+  Widget getBody() {
+    bool showLoading = songs.isEmpty;
+    if (showLoading) {
+      return getProgressBar();
+    } else {
+      return getListView();
+    }
+  }
+
+  Widget getProgressBar() {
+    return const Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+
+  ListView getListView() {
+    return ListView.separated(
+      itemBuilder: (context, position) {
+        return getRow(position);
+      },
+      separatorBuilder: (context, index) {
+        return const Divider(
+          color: Colors.grey,
+          thickness: 1,
+          indent: 24,
+          endIndent: 24,
+        );
+      },
+      itemCount: songs.length,
+      shrinkWrap: true,
+    );
+  }
+
+  Widget getRow(int index) {
+    return Align(
+      alignment: Alignment.center,
+      child: Text(songs[index].title),
+    );
+  }
+
+  void observeData() {
+    _viewModal.songStream.stream.listen((songList) {
+      setState(() {
+        songs.addAll(songList);
+      });
+    });
   }
 }
---
+++ lib/ui/home/viewmodal.dart
@@ -0,0 +1,14 @@
+import 'dart:async';
+
+import 'package:musicflutter/data/modal/song.dart';
+import 'package:musicflutter/data/responsitory/responsitory.dart';
+class MusicAppViewModal {
+  StreamController<List<Song>> songStream = StreamController();
+  void loadSong()
+  {
+    final repository = DefaultRepository();
+    repository.loadData().then((value) => songStream.add(value!));
+  }
+
+
+}
