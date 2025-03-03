2f54a3844672945b3cb7e32b27d2b05760a3ba27
Your Name <nguyenhoainama3k55@gmail.com>
2025-03-03 14:56:58 +0700

    .

--- lib/ui/home/home.dart
+++ lib/ui/home/home.dart
@@ -6,66 +6,6 @@ import 'package:musicflutter/ui/home/viewmodal.dart';
 import 'package:musicflutter/ui/now_playing/playing.dart';
 import 'package:musicflutter/ui/now_playing/audio_player_manager.dart';
 
-// class MusicAppp extends StatelessWidget {
-//   const MusicAppp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Music App',
-//       theme: ThemeData(
-//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
-//         useMaterial3: true,
-//       ),
-//       home: const MusicHomePage(),
-//       debugShowCheckedModeBanner: false,
-//     );
-//   }
-// }
-
-// class MusicHomePage extends StatefulWidget {
-//   const MusicHomePage({super.key});
-
-//   @override
-//   State<MusicHomePage> createState() => _MusicHomePageState();
-// }
-
-// class _MusicHomePageState extends State<MusicHomePage> {
-//   final List<Widget> _tabs = [
-//     const HomeTab(),
-//     const DiscoveryTab(),
-//     const AccountTab(),
-//     const SettingsTab(),
-//   ];
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return CupertinoPageScaffold(
-//       navigationBar: const CupertinoNavigationBar(middle: Text('Music App')),
-//       child: CupertinoTabScaffold(
-//         tabBar: CupertinoTabBar(
-//           backgroundColor: Theme.of(context).colorScheme.onInverseSurface,
-//           items: const [
-//             BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-//             BottomNavigationBarItem(
-//               icon: Icon(Icons.album),
-//               label: 'Discovery',
-//             ),
-//             BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Account'),
-//             BottomNavigationBarItem(
-//               icon: Icon(Icons.settings),
-//               label: 'Settings',
-//             ),
-//           ],
-//         ),
-//         tabBuilder: (BuildContext context, int index) {
-//           return _tabs[index];
-//         },
-//       ),
-//     );
-//   }
-// }
-
 class HomeTab extends StatelessWidget {
   const HomeTab({super.key});
 
